c2bddbfa4369c5bf4de775ec3104aa33b82c16ba
oipik <wazuup.lw@mail.ru>
2023-09-04 22:21:16 +0300

    main.js add

--- app/js/main.js
+++ app/js/main.js
@@ -4,58 +4,117 @@ document.addEventListener('DOMContentLoaded', () => {
         questions = document.querySelectorAll('.wrapper__question'),
         currentQuestion = document.querySelector('.current'),
         totalQuestions = document.querySelector('.total'),
-        nextQuestion = document.querySelector('.next'),
+        nextQuestionBtn = document.querySelector('.next'),
+        buttonsBox = document.querySelector('.wrapper__buttons'),
+        allAnswersShowBtn = document.querySelector('.wrapper__btn-answers'),
+        startAgainBtn = document.querySelector('.wrapper__btn-again'),
+
+        quizRezult = document.querySelector('.wrapper__quizResult'),
+        quizScore = document.querySelector('.wrapper__quizScore span'),
+        quizLevel = document.querySelector('.wrapper__quizLevel span'),
+        textAbout = document.querySelector('.wrapper__text'),
+        inputs = document.querySelectorAll('.wrapper__answers-item input'),
+
+        answersAll = [],
+        answersGet = [],
+        answerShowOnPage,
+        numberOfAnswers = 0;
         index = 0;
 
-    currentQuestion.textContent = index + 1;
+    answersAll = ['Да', '// Это комментарий.', 'Math.round(3.14)',];
+
     totalQuestions.textContent = questions.length;
 
     btnStart.addEventListener('click', () => {
         btnStart.style.display = 'none';
         questionsMain.style.display = 'block';
-        nextQuestion.style.display = 'block';
-        showQuestion();
+        nextQuestionBtn.style.display = 'block';
+        showQuestion(index);
     })
 
-    nextQuestion.addEventListener('click', () => {
-        let counter = 0;
-        console.log('hello')
-        questions.forEach(item => {
-            if(item.style.display === 'block') {
-                document.querySelectorAll('.wrapper__answers-item input').forEach(item => {
-                    if(item.checked) {
-                        counter++;
+    nextQuestionBtn.addEventListener('click', () => {
+        let count = 0;
+
+        questions.forEach((item, i) => {
+            if (item.style.display == 'block') {
+                inputs.forEach(input => {
+                    if (input.checked) {
+                        count++;
+                        let value = document.querySelector(`label[for="${input.id}"]`).textContent;
+                        answerShowOnPage = input.closest('ul').nextElementSibling;
+                        addAnswer(i, value, answerShowOnPage);
+                        input.checked = false;
+                        return;
                     }
-                }) 
-                
-                if(counter == 0) {
-                    alert('Вы должны выбрать хотя бы один ответ.');
-                    return;
-                } else {
-                    index++;
-                    hideAllQuestions();
-                    showQuestion();
-                    showCurrentQuestion()
-                }
+                })
             }
         })
+
+        if (count === 0) {
+            alert('Вы должны выбрать хотя бы один ответ.');
+            return;
+        }
+
+        questions.forEach(item => item.style.display = 'none');
+        index++;
+
+        showQuestion(index);
     });
 
-    function hideAllQuestions() {
-        questions.forEach(item => item.style.display = 'none');
+    function addAnswer(index, value, elem) {
+        answersGet[index] = value;
+
+        if (answersGet[index] == answersAll[index]) {
+            numberOfAnswers++;
+            elem.textContent = 'Правильно';
+            elem.classList.add('correct');
+        } else {
+            elem.textContent = 'Неправильно';
+            elem.classList.add('incorrect');
+        }
     }
 
-    function showQuestion() {
+    function showQuestion(index) {
         questions.forEach((item, i) => {
-            if(index == i) {
+            if (index == i) {
                 item.style.display = 'block';
-            } 
+            }
         });
-        index++;
+
+        if (index <= questions.length - 1) {
+            currentQuestion.textContent = index + 1;
+        } else {
+            showResult();
+        }
     }
 
-    function showCurrentQuestion() {
-        currentQuestion.textContent = index + 1;
+    function showResult() {
+        questionsMain.style.display = 'none';
+        textAbout.style.display = 'none';
+        nextQuestionBtn.style.display = 'none';
+        quizScore.textContent = numberOfAnswers + ' / ' + questions.length;
+        quizRezult.style.display = 'block';
+        buttonsBox.style.display = 'block';
+        if (numberOfAnswers >= 0 || numberOfAnswers <= 17) {
+            quizLevel.textContent = 'К сожалению Вы не набрали достаточное количество балов для сдачи теста. Попробуйте снова';
+        } else if (numberOfAnswers > 17 || numberOfAnswers <= 25) {
+            quizLevel.textContent = 'Вы почти почти набрали необходимое количество баллов. Попробуйте еще раз';
+        } else {
+            quizLevel.textContent = 'Вы ответили на все вопросы. Молодец!';
+        }
     }
-    
+
+    allAnswersShowBtn.addEventListener('click', () => {
+        questionsMain.style.display = 'block';
+        questions.forEach(item => item.style.display = 'block');
+    });
+
+    startAgainBtn.addEventListener('click', () => {
+        quizRezult.style.display = 'none';
+        buttonsBox.style.display = 'none';
+        btnStart.style.display = 'block';
+        questionsMain.style.display = 'none';
+        nextQuestionBtn.style.display = 'none';
+    });
+
 })
